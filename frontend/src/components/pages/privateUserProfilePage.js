f21c4c86de9194d3aa5acbb7304b488ae2167fb0
Yogesh Acharya <yogeshacharya@Poonams-MacBook-Pro.local>
2023-04-10 23:05:46 -0400

    add stuff

--- frontend/src/components/pages/privateUserProfilePage.js
+++ frontend/src/components/pages/privateUserProfilePage.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Image,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useNavigate } from "react-router-dom";
 import getUserInfo from "../../utilities/decodeJwt";
 
-
-//link to service
-//http://localhost:3000/privateUserProfile
-
 const PrivateUserProfile = () => {
   const [show, setShow] = useState(false);
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const navigate = useNavigate();
-
 
   // handle logout button
   const handleLogout = (async) => {
@@ -23,17 +27,62 @@ const PrivateUserProfile = () => {
   };
 
   useEffect(() => {
-    setUser(getUserInfo())
+    setUser(getUserInfo());
   }, []);
 
-
-  // 	<span><b>{<FollowerCount username = {username}/>}</b></span>&nbsp;
-  // <span><b>{<FollowingCount username = {username}/>}</b></span>;
-  if (!user) return (<div><h4>Log in to view this page.</h4></div>)
+  if (!user)
+    return (
+      <div>
+        <h4>Log in to view this page.</h4>
+      </div>
+    );
   return (
-    <div class="container">
-      <div class="col-md-12 text-center">
-        <h1>{user && user.username}</h1>
+    <section style={{ backgroundColor: "#f4f5f7" }}>
+      <Container className="py-5 h-100">
+        <Row className="justify-content-center align-items-center h-100">
+          <Col lg="6" className="mb-4 mb-lg-0">
+            <Card className="mb-3" style={{ borderRadius: ".5rem" }}>
+              <Row className="g-0">
+                <Col
+                  md="4"
+                  className="gradient-custom text-center text-white"
+                  style={{
+                    borderTopLeftRadius: ".5rem",
+                    borderBottomLeftRadius: ".5rem",
+                  }}
+                >
+                  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0" y="0" width="100" height="100" fill="#fff" />
+                    <circle cx="50" cy="50" r="45" fill="#aaa" />
+                    <text
+                      x="50"
+                      y="55"
+                      font-size="30"
+                      fill="#fff"
+                      text-anchor="middle"
+                      dominant-baseline="middle"
+                    >
+                      { user.username ? user.username[0].toUpperCase() : "Guest"}
+                    </text>
+                  </svg>
+                </Col>
+                <Col md="8">
+                  <Card.Body className="p-4">
+                    <h6>Information</h6>
+                    <hr className="mt-0 mb-4" />
+                    <Row className="pt-1">
+                      <Col size="6" className="mb-3">
+                        <h6>Username</h6>
+                        <p className="text-muted">{user.username}</p>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+
         <div class="col-md-12 text-center">
           <>
             <Button className="me-2" onClick={handleShow}>
@@ -60,8 +109,8 @@ const PrivateUserProfile = () => {
             </Modal>
           </>
         </div>
-      </div>
-    </div>
+      </Container>
+    </section>
   );
 };
 
